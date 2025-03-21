604dfc25020238a50dd1436525c88cbc032d1d26
Mario G. <ma.gomez@duocuc.cl>
2025-03-21 19:21:41 -0300

    Initial commit (via bun create)

--- src/main/java/cl/pixelcode/back/service/PixelcodeService.java
+++
@@ -1,8 +0,0 @@
-package cl.pixelcode.back.service;
-
-import cl.pixelcode.back.dto.request.ContactoRequestDto;
-import cl.pixelcode.back.dto.response.ResponseDto;
-
-public interface PixelcodeService {
-    ResponseDto registrarContacto(ContactoRequestDto contacto);
-}
