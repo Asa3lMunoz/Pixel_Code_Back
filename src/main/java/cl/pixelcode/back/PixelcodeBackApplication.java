a991ceabaf16a07899ea40738a3c088d2839ecc7
Mario G. <ma.gomez@duocuc.cl>
2025-03-20 21:40:26 -0300

    Initial commit

---
+++ src/main/java/cl/pixelcode/back/PixelcodeBackApplication.java
@@ -0,0 +1,13 @@
+package cl.pixelcode.back;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PixelcodeBackApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PixelcodeBackApplication.class, args);
+    }
+
+}
---
+++ src/test/java/cl/pixelcode/back/PixelcodeBackApplicationTests.java
@@ -0,0 +1,13 @@
+package cl.pixelcode.back;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PixelcodeBackApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
