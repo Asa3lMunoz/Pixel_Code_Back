8edc014c983fc220762ff15465ea2f068c956e5a
Mario G. <ma.gomez@duocuc.cl>
2025-03-23 21:31:11 -0300

    Incorporadas dependencias de Docker. Actualizado package.json

--- src/index.ts
+++ src/index.ts
@@ -19,7 +19,7 @@ const app = new Elysia({
     .use(pricingPlansRoutes)
     .use(servicesRoutes)
     .use(usersRoutes)
-    .listen(3000);
+    .listen(Bun.env.PORT ?? 3000);
 
 console.log(
     `🦊 Elysia is running at ${app.server?.hostname}:${app.server?.port}`
